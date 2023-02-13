4d5a53d3a1556a2a98f1caa59f11ead28d3d6513
mateusvg <mateusvitorino@outlook.com.br>
2023-02-13 16:48:23 -0300

    pagina de status  e tabela, e alteraçoes icones

--- src/Pages/Table.tsx
+++ src/Pages/Table.tsx
@@ -1,67 +1,71 @@
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+import {CircleIcon} from '../Components/CircleIconStatus'
+import { SettingsIcon, DownloadIcon } from '@chakra-ui/icons'
+import { Button } from '@chakra-ui/react';
 
-
-  
 export default function Simple() {
 
 
-    return (
-        <>
-        <TableContainer>
-  <Table variant='striped' colorScheme='teal'>
-    <TableCaption>Pacientes</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>Nome</Th>
-        <Th>CPF</Th>
-        <Th>Anexo</Th>
-        <Th>Médico</Th>
-        <Th>Aptidão</Th>
-        <Th>Status</Th>
-        <Th>Ação</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>Tobias Silva</Td>
-        <Td>242.417.550-03</Td>
-        <Td>inches</Td>
-        <Td>Jose Maria</Td>
-        <Td>Apto</Td>
-        <Td>Em análise</Td>
-        <Td>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>Mateus Manfredo</Td>
-        <Td>485.815.450-50</Td>
-        <Td>inches</Td>
-        <Td>Jose Maria</Td>
-        <Td>Inapto</Td>
-        <Td>Reprovado</Td>
-        <Td >30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>Natalia Amorin</Td>
-        <Td>464.802.400-14</Td>
-        <Td>inches</Td>
-        <Td>Jose Maria</Td>
-        <Td>Apto</Td>
-        <Td>Aprovado</Td>
-        <Td >0.91444</Td>
-      </Tr>
-    </Tbody>
-  </Table>
-</TableContainer>
-        </>
-    )
+  return (
+    <>
+      <TableContainer>
+        <Table variant='striped' colorScheme='teal'>
+          <TableCaption>Pacientes</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Nome</Th>
+              <Th>CPF</Th>
+              <Th>Anexo</Th>
+              <Th>Médico</Th>
+              <Th>Aptidão</Th>
+              <Th>Status</Th>
+              <Th></Th>
+              <Th>Ação</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Tobias Silva</Td>
+              <Td>242.417.550-03</Td>
+              <Td><Button><DownloadIcon /></Button></Td>
+              <Td>Jose Maria</Td>
+              <Td>Apto</Td>
+              <Td>Em análise </Td>
+              <Td><CircleIcon color='blue.500' /></Td>
+              <Td><Button><SettingsIcon /></Button></Td>
+            </Tr>
+            <Tr>
+              <Td>Mateus Manfredo</Td>
+              <Td>485.815.450-50</Td>
+              <Td><Button><DownloadIcon /></Button></Td>
+              <Td>Jose Maria</Td>
+              <Td>Inapto</Td>
+              <Td>Reprovado</Td>
+              <Td><CircleIcon color='red.500' /></Td>
+              <Td><Button><SettingsIcon /></Button></Td>
+            </Tr>
+            <Tr>
+              <Td>Natalia Amorin</Td>
+              <Td>464.802.400-14</Td>
+              <Td><Button><DownloadIcon /></Button></Td>
+              <Td>Jose Maria</Td>
+              <Td>Apto</Td>
+              <Td>Aprovado</Td>
+              <Td><CircleIcon color='green.500' /></Td>
+              <Td><Button><SettingsIcon /></Button></Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  )
 }
