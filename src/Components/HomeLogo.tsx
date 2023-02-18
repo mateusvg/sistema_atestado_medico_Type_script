20962fea9f22b9b9cb2840ec48c27c5afe4e1852
mateusvg <mateusvitorino@outlook.com.br>
2023-02-18 10:29:02 -0300

    center logo

--- src/Components/HomeLogo.tsx
+++ src/Components/HomeLogo.tsx
@@ -2,13 +2,14 @@ import { Center, Text, Stack } from '@chakra-ui/react'
 import LogoX from '../img/LogoX.png'
 export default function () {
     return (
-        <Center h='100px' color='white' mt={270}>
-            <Stack spacing={5} direction='column' justify='center' mt={4}>
+        <Center >
+            <Stack spacing={5} direction='column' justify='center' mt={7}>
                 <Center>
-
-                <Text  color='tomato' >Sistema de agendamento e atestado médico</Text>
+                    <Text color='tomato' >Sistema de agendamento e atestado médico</Text>
                 </Center>
-                <img src={LogoX} alt="Logo" />
+                <Center>
+                    <img src={LogoX} alt="Logo" width={"40%"} />
+                </Center>
             </Stack>
         </Center>
     )
