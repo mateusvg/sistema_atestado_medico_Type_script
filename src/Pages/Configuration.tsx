5dfe75ae357e4792317398be3945d0e7c811523b
mateusvg <mateusvitorino@outlook.com.br>
2023-02-23 14:12:10 -0300

    configuration page inputs

--- src/Pages/Configuration.tsx
+++ src/Pages/Configuration.tsx
@@ -1,7 +1,50 @@
 import {
+    Center,
+    Stack,
+    InputGroup,
+    InputLeftAddon,
+    Input,
+    Button
 
-    Center
-} from '@chakra-ui/react';
+} from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons';
+import { ChangeEvent, useState } from 'react';
+
 export default function Simple() {
-    return(<Center mt={10}>Configuration Page</Center>)
+    const [state, setState] = useState<'initial' | 'submitting' | 'success'>(
+        'initial'
+    );
+    const [senha, setSenha]= useState('')
+    return (
+        <Stack mt={10}>
+            <Center mb={5}>Configurações</Center>
+            <Center>
+                <Stack spacing={4} mr={10}>
+                    <InputGroup>
+                        <InputLeftAddon children='Usuário:' />
+                        <Input type='tel' placeholder='Usuário' required />
+                    </InputGroup>
+
+                    <InputGroup>
+                        <InputLeftAddon children='Senha:' />
+                        <Input
+                            type='password'
+                            placeholder='Senha'
+                            required
+                            disabled={state !== 'initial'}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                setSenha(e.target.value)
+                            } />
+                    </InputGroup>
+                    <Button
+                        colorScheme={state === 'success' ? 'green' : 'blue'}
+                        isLoading={state === 'submitting'}
+                        w="100%"
+                        type={state === 'success' ? 'button' : 'submit'}>
+                        {state === 'success' ? <CheckIcon /> : 'Alterar'}
+                    </Button>
+                </Stack>
+            </Center>
+        </Stack>
+    )
 }
