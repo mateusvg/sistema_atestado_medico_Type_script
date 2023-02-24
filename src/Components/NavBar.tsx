28e37bc4a5840a57bef88ed668d1ce8a06b6f3fe
mateusvg <mateusvitorino@outlook.com.br>
2023-02-23 21:55:24 -0300

    navigation collapse context

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -115,7 +115,7 @@ export default function WithSubnavigation() {
                         }}>
                         Agendamento
                     </Button>
-                </Stack> : <> </> }
+                </Stack> : <> </>}
 
                 <Stack
                     flex={{ base: 1, md: 0 }}
@@ -147,91 +147,70 @@ export default function WithSubnavigation() {
     );
 }
 
-const MobileNav = (props:any) => {
+const MobileNav = () => {
     return (
         <Stack
             bg={useColorModeValue('white', 'gray.800')}
             p={4}
             display={{ md: 'none' }}>
-            {NAV_ITEMS.map((navItem) => (
-                <MobileNavItem key={navItem.label} {...navItem } />
-            ))}
+
+            <MobileNavItem />
+
         </Stack>
     );
 };
 
-const MobileNavItem = ({ label, children, href }: NavItem, props:any) => {
-    const { isOpen, onToggle } = useDisclosure();
+const MobileNavItem = () => {
+    const { isOpen, onToggle, onClose } = useDisclosure();
+
+    const navigate = useNavigate();
+    const { context, setContext } = useContext(Context);
+    const color = useColorModeValue('gray.600', 'gray.200')
+    
     return (
-        <Stack spacing={4} onClick={children && onToggle}>
+        <Stack spacing={4} onClick={onToggle}>
             <Flex
                 py={2}
                 as={Link}
-                href={href ?? '#'}
-                justify={'space-between'}
+                flexDirection={"column"}
                 align={'center'}
                 _hover={{
                     textDecoration: 'none',
                 }}>
                 <Text
+                    m={2}
                     fontWeight={600}
-                    color={useColorModeValue('gray.600', 'gray.200')}>
-                    {label}
+                    color={color}
+                    onClick={() => context ? navigate('/login/table') : navigate('login')}
+                >
+                    Admin
                 </Text>
-                {children && (
-                    <Icon
-                        as={ChevronDownIcon}
-                        transition={'all .25s ease-in-out'}
-                        transform={isOpen ? 'rotate(180deg)' : ''}
-                        w={6}
-                        h={6}
-                    />
-                )}
+                {context ?
+                    <Text
+                        m={2}
+                        fontWeight={600}
+                        color={color}
+                        onClick={() => navigate('/login/calendar')}
+                    >
+                        Agendamento
+                    </Text> : <></>}
+                <Text
+                    m={2}
+                    fontWeight={600}
+                    color={color}
+                    onClick={() => navigate('/form')}
+                >
+                    Formulário
+                </Text>
+                <Text
+                    m={2}
+                    fontWeight={600}
+                    color={color}
+                    onClick={() => navigate('/status')}
+                >
+                    Status
+                </Text>
             </Flex>
-
-            <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-                <Stack
-                    mt={2}
-                    pl={4}
-                    borderLeft={1}
-                    borderStyle={'solid'}
-                    borderColor={useColorModeValue('gray.200', 'gray.700')}
-                    align={'start'}>
-                    {children &&
-                        children.map((child) => (
-                            <Link key={child.label} py={2} href={child.href}>
-                                {child.label}
-                            </Link>
-                        ))}
-                </Stack>
-            </Collapse>
         </Stack>
     );
 };
-
-interface NavItem {
-    label: string;
-    subLabel?: string;
-    children?: Array<NavItem>;
-    href?: string;
-}
-
-const NAV_ITEMS: Array<NavItem> = [
-    {
-        label: 'Admin',
-        href: '/login',
-    },
-    {
-        label: 'Formulario',
-        href: '/form',
-    },
-    {
-        label: 'Status',
-        href: '/status',
-    },
-    {
-        label: 'Agendamento',
-        href: '/login/calendar',
-    },
-    
-];
