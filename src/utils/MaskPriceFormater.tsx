b602a24ae5ed080fcffefec2ce2e32d3599fb796
mateusvg <mateusvitorino@outlook.com.br>
2023-03-02 13:51:43 -0300

    mask prince adjustments

--- src/utils/MaskPriceFormater.tsx
+++ src/utils/MaskPriceFormater.tsx
@@ -8,11 +8,12 @@ export const currency = (value: any) => {
         var tmp = int + '';
 
         if(tmp.length == 1) tmp = "00"+tmp
+        if(tmp.length == 2) tmp = "0"+tmp
 
         tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
         if (tmp.length > 6)
             tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-            if(tmp.indexOf(".") == 0) tmp = tmp.replace(".","");
+            if(tmp.indexOf(".") == 0) tmp = "00"+tmp
         return tmp;
     }
     let teste = getMoney(value)
