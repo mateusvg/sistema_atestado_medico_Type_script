8396862f415d78a3983f4858b0a9755fc7f3f1c3
mateusvg <mateusvitorino@outlook.com.br>
2023-03-06 14:56:24 -0300

    reports layout

--- src/Pages/Reports.tsx
+++ src/Pages/Reports.tsx
@@ -1,3 +1,77 @@
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+    Button, 
+    Center
+} from '@chakra-ui/react'
+
 export default function Reports() {
-    return (<>Reports</>)
+    return (
+
+        <Box ml ={"25%"}w={"45%"}>
+   
+            <Accordion>
+
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Estoque
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel>
+                        <Button >
+                            Total em estoque
+                        </Button>
+                    </AccordionPanel>
+                    <AccordionPanel>
+                        <Button >
+                            Estoque detalhado
+                        </Button>
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Vendas
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Button >
+                            Total em vendas
+                        </Button>
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Funcionarios
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel>
+                        <Button >
+                            Relação Status/Funcionários
+                        </Button>
+                    </AccordionPanel>
+                </AccordionItem>
+
+            </Accordion>
+
+        </Box>
+    )
 }
