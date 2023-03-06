6ddf15a45cd8c1784fcc13c69df02c95e8c0d3f1
mateusvg <mateusvitorino@outlook.com.br>
2023-03-06 14:39:20 -0300

    query to subtract product from stock

--- src/Api/src/models/adminSaleModel.js
+++ src/Api/src/models/adminSaleModel.js
@@ -3,7 +3,8 @@ const conn = require('../db/connection')
 async function saleInsert(values) {
     console.log(JSON.stringify(values[0][0].idStock))
     let idStock = values[0][0].idStock
-    let idSales =0
+    let idSales = 0
+    let quantidade = -1
     try {
         const result = await new Promise((resolve, reject) => {
             conn.query('INSERT into sales  VALUES (?, ?, ?)',[ idSales, 1,idStock ], (error, results, fields) => {
@@ -11,7 +12,14 @@ async function saleInsert(values) {
                 return resolve(results);
             });
         });
+        const updateTableStock = await new Promise((resolve, reject) => {
+            conn.query('update stock set  quantidade = quantidade ?  where idStock = ?',[ quantidade , idStock ], (error, results, fields) => {
+                if (error) return reject(error);
+                return resolve(results);
+            });
+        });
         console.log(result)
+        console.log(`update stock table ${updateTableStock}`)
         let tratado = JSON.stringify(result)
         return tratado
     } catch (err) {
