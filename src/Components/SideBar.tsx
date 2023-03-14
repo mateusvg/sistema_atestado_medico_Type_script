87640eb04c7a0391ada7a42aaa3985947c7f04a4
mateusvg <mateusvitorino@outlook.com.br>
2023-03-14 09:09:08 -0300

    icon admin

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -235,16 +235,14 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
                             <HStack>
                                 <Avatar
                                     size={'sm'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                    }
+                                    src={'https://bit.ly/broken-link'}
                                 />
                                 <VStack
                                     display={{ base: 'none', md: 'flex' }}
                                     alignItems="flex-start"
                                     spacing="1px"
                                     ml="2">
-                                    <Text fontSize="sm">Justina Clark</Text>
+                                    <Text fontSize="sm">Admin</Text>
                                     <Text fontSize="xs" color="gray.600">
                                         Admin
                                     </Text>
